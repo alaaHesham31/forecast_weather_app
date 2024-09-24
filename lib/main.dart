a7cc5dbd4ca95f614b055fa45a0bdbb90c760073
alaa <alaa.elkeshky33@gmail.com>
2024-09-25 00:54:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:forcast_weather_app/screens/home_screen.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: HomeScreen(),
+
+    );
+  }
+}
---
+++ lib/screens/forecast_screen.dart
@@ -0,0 +1,165 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:forcast_weather_app/services/weather_service.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ForecastScreen extends StatefulWidget {
+  final String city;
+  const ForecastScreen({required this.city});
+
+  @override
+  State<ForecastScreen> createState() => _ForecastScreenState();
+}
+
+class _ForecastScreenState extends State<ForecastScreen> {
+  final WeatherService _weatherService = WeatherService();
+  List<dynamic>? _forecast;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchForecast();
+  }
+
+  Future<void> _fetchForecast() async {
+    try {
+      final forecastData = await _weatherService.fetch7DayForecast(widget.city);
+      setState(() {
+        _forecast = forecastData['forecast']['forecastday'];
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: _forecast == null
+            ? Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF1A2344),
+                      Color.fromARGB(255, 125, 32, 142),
+                      Colors.purple,
+                      Color.fromARGB(255, 151, 44, 170),
+                    ],
+                  ),
+                ),
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            : Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF1A2344),
+                      Color.fromARGB(255, 125, 32, 142),
+                      Colors.purple,
+                      Color.fromARGB(255, 151, 44, 170),
+                    ],
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                            SizedBox(width: 15),
+                            Text(
+                              '7 Days Forecast',
+                              style: GoogleFonts.lato(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          final day = _forecast![index];
+                          String iconUrl =
+                              'http:${day['day']['condition']['icon']}';
+                          return Padding(
+                            padding: EdgeInsets.all(10),
+                            child: ClipRRect(
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                                child: Container(
+                                  height: 110,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    gradient: LinearGradient(
+                                      begin: AlignmentDirectional.topStart,
+                                      end: AlignmentDirectional.bottomEnd,
+                                      colors: [
+                                        Color(0xFF1A2344).withOpacity(0.5),
+                                        Color(0xFF1A2344).withOpacity(0.2),
+                                      ],
+                                    ),
+                                  ),
+                                  child: ListTile(
+                                    leading: Image.network(iconUrl),
+                                    title: Text(
+                                      '${day['date']} \n ${day['day']['avgtemp_c'].round()} °C',
+                                      style: GoogleFonts.lato(
+                                        fontSize: 22,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    subtitle: Text(day['day']['condition']['text'],
+                                    style: GoogleFonts.lato(
+                                      fontSize: 18,
+                                      color: Colors.white70,
+                                    ),),
+                                     trailing: Text(
+                                      'Max: ${day['day']['maxtemp_c']} °C\n Min: ${day['day']['mintemp_c']} °C',
+                                      style: GoogleFonts.lato(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: _forecast!.length,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+}
